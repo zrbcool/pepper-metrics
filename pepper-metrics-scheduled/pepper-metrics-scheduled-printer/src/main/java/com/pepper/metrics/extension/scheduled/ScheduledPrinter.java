9b196410759ee25e38f3f8ff78a5ace6d9e495a0
Lord_X_ <xzmrainbow@gmail.com>
2019-08-06 20:02:48 +0800

    begin

--- pepper-metrics-scheduled/pepper-metrics-scheduled-printer/src/main/java/com/pepper/metrics/extension/scheduled/ScheduledPrinter.java
+++ pepper-metrics-scheduled/pepper-metrics-scheduled-printer/src/main/java/com/pepper/metrics/extension/scheduled/ScheduledPrinter.java
@@ -3,13 +3,22 @@ package com.pepper.metrics.extension.scheduled;
 import com.pepper.metrics.core.ScheduledRun;
 import com.pepper.metrics.core.Stats;
 import com.pepper.metrics.core.extension.SpiMeta;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Set;
 
 @SpiMeta(name = "printer")
 public class ScheduledPrinter implements ScheduledRun {
+
+    private static final Logger pLogger = LoggerFactory.getLogger("performance");
+
     @Override
     public void run(Set<Stats> statsSet) {
+        for (Stats stats : statsSet) {
+        }
+
+
         System.out.println(statsSet);
     }
 }
