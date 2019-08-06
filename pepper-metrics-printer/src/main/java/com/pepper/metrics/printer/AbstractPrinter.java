2f3554049c1d1e51f2e5f8c5ffc04721b7160a47
Lord_X_ <xzmrainbow@gmail.com>
2019-08-06 14:30:59 +0800

    同步复刻备份

---
+++ pepper-metrics-printer/src/main/java/com/pepper/metrics/printer/AbstractPrinter.java
@@ -0,0 +1,13 @@
+package com.pepper.metrics.printer;
+
+/**
+ * Description:
+ *  通用模板
+ * @author zhiminxu
+ * @package com.pepper.metrics.printer
+ * @create_time 2019-08-06
+ */
+public abstract class AbstractPrinter {
+
+
+}
