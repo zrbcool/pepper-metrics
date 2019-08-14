1bf406d788bcfc068ecf491a6f9401ddcb9d4170
Lord_X_ <xzmrainbow@gmail.com>
2019-08-14 16:54:07 +0800

    motan sample in jvm

--- pepper-metrics-samples/motan-sample-jvm/src/main/java/com/pepper/metrics/sample/motan/HelloServiceImpl.java
+++ pepper-metrics-samples/motan-sample-jvm/src/main/java/com/pepper/metrics/sample/motan/HelloServiceImpl.java
@@ -1,5 +1,7 @@
 package com.pepper.metrics.sample.motan;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * Description:
  *
@@ -10,6 +12,12 @@ package com.pepper.metrics.sample.motan;
 public class HelloServiceImpl implements HelloService {
     @Override
     public void sayHello(String name) {
-        System.out.println("Hello " + name + " !");
+        int waitTime = ThreadLocalRandom.current().nextInt(1000);
+        try {
+            Thread.sleep(waitTime);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Hello " + name + " ! Wait time is " + waitTime);
     }
 }
---
+++ pepper-metrics-samples/motan-sample-jvm/src/main/java/com/pepper/metrics/sample/motan/MotanRpcServer.java
@@ -0,0 +1,56 @@
+package com.pepper.metrics.sample.motan;
+
+import com.weibo.api.motan.common.MotanConstants;
+import com.weibo.api.motan.config.ProtocolConfig;
+import com.weibo.api.motan.config.RegistryConfig;
+import com.weibo.api.motan.config.ServiceConfig;
+import com.weibo.api.motan.util.MotanSwitcherUtil;
+
+/**
+ * Description:
+ *
+ * @author zhiminxu
+ * @package com.pepper.metrics.sample.motan
+ * @create_time 2019-08-14
+ */
+public class MotanRpcServer {
+    public static void main(String[] args) throws InterruptedException {
+        ServiceConfig<HelloService> motanDemoService = new ServiceConfig<HelloService>();
+
+        // 设置接口及实现类
+        motanDemoService.setInterface(HelloService.class);
+        motanDemoService.setRef(new HelloServiceImpl());
+
+        // 配置服务的group以及版本号
+        motanDemoService.setGroup("motan-demo-rpc");
+        motanDemoService.setVersion("1.0");
+
+        // 配置注册中心直连调用
+        RegistryConfig registry = new RegistryConfig();
+
+        //use local registry
+        registry.setRegProtocol("local");
+
+        // use ZooKeeper registry
+//        registry.setRegProtocol("zookeeper");
+//        registry.setAddress("127.0.0.1:2181");
+
+        // registry.setCheck("false"); //是否检查是否注册成功
+        motanDemoService.setRegistry(registry);
+
+        // 配置RPC协议
+        ProtocolConfig protocol = new ProtocolConfig();
+        protocol.setId("motan");
+        protocol.setName("motan");
+        motanDemoService.setProtocol(protocol);
+
+        protocol.setFilter("pepperProfiler");
+
+        motanDemoService.setExport("motan:8002");
+        motanDemoService.export();
+
+        MotanSwitcherUtil.setSwitcherValue(MotanConstants.REGISTRY_HEARTBEAT_SWITCHER, true);
+
+        System.out.println("server start...");
+    }
+}
