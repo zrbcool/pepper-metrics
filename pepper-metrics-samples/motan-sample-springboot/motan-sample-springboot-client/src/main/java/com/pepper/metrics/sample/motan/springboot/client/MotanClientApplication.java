045f4abd44a5e026fcda2b02015d43fcf4bc11a5
Lord_X_ <xzmrainbow@gmail.com>
2019-08-14 18:13:27 +0800

    motan sample with springboot

---
+++ pepper-metrics-samples/motan-sample-springboot/motan-sample-springboot-client/src/main/java/com/pepper/metrics/sample/motan/springboot/client/MotanClientApplication.java
@@ -0,0 +1,46 @@
+package com.pepper.metrics.sample.motan.springboot.client;
+
+import com.pepper.metrics.sample.motan.springboot.api.HelloService;
+import com.weibo.api.motan.config.springsupport.AnnotationBean;
+import com.weibo.api.motan.config.springsupport.annotation.MotanReferer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Scheduled;
+
+/**
+ * Description:
+ *
+ * @author zhiminxu
+ * @package com.pepper.metrics.sample.motan.springboot
+ * @create_time 2019-08-14
+ */
+@SpringBootApplication
+public class MotanClientApplication {
+
+    @MotanReferer(basicReferer = "motantestClientBasicConfig", group = "testgroup", directUrl = "127.0.0.1:8002")
+    private HelloService service;
+
+    @Scheduled(cron = "0/1 * * * * ?")
+    public void timer() {
+        String name = service.sayHello("hahaha");
+        System.out.println(name);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotanClientApplication.class, args);
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Bean
+    public AnnotationBean motanAnnotationBean() {
+        AnnotationBean motanAnnotationBean = new AnnotationBean();
+        motanAnnotationBean.setPackage("com.pepper.metrics.sample.motan.springboot.client");
+        return motanAnnotationBean;
+    }
+
+}
---
+++ pepper-metrics-samples/motan-sample-springboot/motan-sample-springboot-server/src/main/java/com/pepper/metrics/sample/motan/springboot/server/MotanServerApplication.java
@@ -0,0 +1,31 @@
+package com.pepper.metrics.sample.motan.springboot.server;
+
+import com.weibo.api.motan.common.MotanConstants;
+import com.weibo.api.motan.config.springsupport.AnnotationBean;
+import com.weibo.api.motan.util.MotanSwitcherUtil;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * Description:
+ *
+ * @author zhiminxu
+ * @package com.pepper.metrics.sample.motan.springboot
+ * @create_time 2019-08-14
+ */
+@SpringBootApplication
+public class MotanServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotanServerApplication.class, args);
+        MotanSwitcherUtil.setSwitcherValue(MotanConstants.REGISTRY_HEARTBEAT_SWITCHER, true);
+    }
+
+    @Bean
+    public AnnotationBean motanAnnotationBean() {
+        AnnotationBean motanAnnotationBean = new AnnotationBean();
+        motanAnnotationBean.setPackage("com.pepper.metrics.sample.motan.springboot.server");
+        return motanAnnotationBean;
+    }
+}
