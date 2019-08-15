e28b1e2ef7b2e05f28208373fff9db6e3a68907e
Lord_X_ <xzmrainbow@gmail.com>
2019-08-15 15:03:57 +0800

    Dubbo集成扩展

---
+++ pepper-metrics-integration/pepper-metrics-dubbo/src/main/java/com/pepper/metrics/integration/dubbo/DubboProviderProfilerFilter.java
@@ -0,0 +1,31 @@
+package com.pepper.metrics.integration.dubbo;
+
+import org.apache.dubbo.common.extension.Activate;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.apache.dubbo.common.constants.CommonConstants.PROVIDER;
+
+/**
+ * Description:
+ *  Dubbo服务提供方profile收集
+ * @author zhiminxu
+ * @package com.pepper.metrics.integration.dubbo
+ * @create_time 2019-08-15
+ */
+@Activate(group = {PROVIDER}, value = "dubboProviderProfiler")
+public class DubboProviderProfilerFilter extends DubboProfilerFilterTemplate {
+    @Override
+    void afterInvoke(String[] tags, long begin, boolean isError) {
+        PROFILER_STAT_IN.observe(System.nanoTime() - begin, TimeUnit.NANOSECONDS, tags);
+        PROFILER_STAT_IN.decConc(tags);
+        if (isError) {
+            PROFILER_STAT_IN.error(tags);
+        }
+    }
+
+    @Override
+    void beforeInvoke(String[] tags) {
+        PROFILER_STAT_IN.incConc(tags);
+    }
+}
